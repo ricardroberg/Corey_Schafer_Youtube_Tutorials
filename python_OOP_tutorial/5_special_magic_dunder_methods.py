826cfc49dda37f7add49aca5fbc3b6b627144916
Ricard Roberg <ricardroberg@gmail.com>
2020-01-06 15:55:56 -0300

    Video 5 complete

---
+++ python_OOP_tutorial/5_special_magic_dunder_methods.py
@@ -0,0 +1,45 @@
+
+class Employee:
+
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@company.com'
+        self.pay = pay
+
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    def __repr__(self):
+        return f"Employee('{self.first}', '{self.last}', '{self.pay}')"
+
+    def __str__(self):
+        return f'{self.fullname()} - {self.email}'
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
+
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+
+print(len(emp_1))
+
+# print(emp_1 + emp_2)
+
+# print(emp_1)
+#
+# print(repr(emp_1))
+# print(str(emp_1))
+#
+# # É O MESMO QUE:
+# print(emp_1.__repr__())
+# print(emp_1.__str__())
