c74cc5d364c1f55e909621e6f7a3424879919295
ricardroberg <ricardroberg@gmail.com>
2020-01-09 12:01:14 -0300

    Video 3 Created the base.html and organized the home.html and about.html, load the default bootstrap, setup menu bar, 'foor loop' for posts.

--- python_django_tutorial/blog/views.py
+++ python_django_tutorial/blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post',
+        'content': 'First post content',
+        'date_posted': 'January 06, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 08, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
