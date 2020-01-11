7c96933b79c36bbdbf5d1198c5519e5c1a2c96d0
ricardroberg <ricardroberg@gmail.com>
2020-01-11 10:11:46 -0300

    Create paths for login and logout on project directory urls.py added LOGIN_REDIRECT_URL = 'blog-home' on project settings Create the path for profile and the user authenticated requirement added LOGIN_URL = 'login' on project settings

--- python_django_tutorial/users/views.py
+++ python_django_tutorial/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+
 
 
 def register(request):
@@ -9,11 +11,15 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             # form = []  # Acho que é necessário para zerar os campos do form.
-            return redirect('blog-home')
+            return redirect('login')
 
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
