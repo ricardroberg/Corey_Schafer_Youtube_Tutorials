d1042df0b9c5648aa2572de6375aa6b1acefd771
Ricard Roberg <ricardroberg@gmail.com>
2020-01-14 14:51:06 -0300

    NEW BRANCH TEST

--- python_django_tutorial/users/views.py
+++ python_django_tutorial/users/views.py
@@ -19,7 +19,7 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
-@login_required
+@login_requiredq
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
