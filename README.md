4bc4074244a495158f8fee65fac3a2d04b33b57c
Ricard Roberg <ricardroberg@gmail.com>
2020-02-15 22:29:23 -0300

    Video2 More about templates and templates heritage, extend base layout html pages and load block contents using 'url_for' on html to redirect to static folder

--- README.md
+++ README.md
@@ -37,3 +37,5 @@ more coming...
 https://www.youtube.com/watch?v=MwZwr5Tvyxo&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH  
 
 1 - Getting Started  
+2 - Templates  
+
--- python_flask_tutorial/flaskblog.py
+++ python_flask_tutorial/flaskblog.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2020'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
+
 
 @app.route("/admin")
 def admin():
